0ef03dcc5b0b0144d779b31c9ffcd4cbbe7eea5f
Marcel Wack <wack@math.tu-berlin.de>
2024-10-01 09:18:49 +0200

    conejcture test

--- computation/conjecture_hpc.jl
+++ computation/conjecture_hpc.jl
@@ -1,8 +1,10 @@
 #Code to Run on the cluster
+using QuantumGB
 
 #n will be set by *.job file
 
-str = check_conjecture(n)
+deg_bound = 8
+str = check_conjecture(n; deg_bound=deg_bound)
 
 # Write the output to a file
 filepath = "../data/conjecture_n_$(n).txt"
