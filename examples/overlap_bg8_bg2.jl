465557c44a53f27761858f760898c2987c5fdbaa
leonard schmitz <leonard.schmitz@rwth-aachen.deleonard.schmitz@rwth-aachen.deleonard.schmitz@rwth-aachen.de>
2025-02-03 21:53:49 +0100

    progress

--- examples/overlap_bg8_bg2.jl
+++ examples/overlap_bg8_bg2.jl
@@ -32,28 +32,26 @@ reduction_string(G1, bg(8,s,t,2,u=u)*u[r,4]*u[w,3]-u[2,s]*u[4,t]*bg(2,3,r,w,u=u)
 +sum(u[3,s]*col_sum(t,u)*u[3,i]*u[r,j]*u[w,k] for i in (3:n) for j in (2:n) for k in (2:n) if i!=j && k!=3 && j!=k && i!=t)
 +sum(u[3,s]*rwel(t,2,u=u)*u[r,b]*u[w,3] for b in (4:n))
 -sum(u[3,s]*rwel(t,a,u=u)*u[r,b]*u[w,c] for a in (3:n) for b in (2:n) for c in (2:n) if a!=t && b!=c && c!=3 && a!=b)
-+sum(u[3,s]*u[2,t]*wel(r,a,b,u=u)*u[w,c] for a in (3:n) for b in (2:n) for c in (2:n) if a!=b && c!=3 && (b,c)!=(2,2))
--sum(u[3,s]*u[2,t]*wel(r,2,b,u=u)*u[w,3] for b in (4:n))
--sum(u[3,s]*u[2,t]*u[c,b]*row_sum(r,u)*u[w,3] for b in (3:n) for c in (4:n) if c != r)
+-sum(u[3,s]*u[a,t]*u[c,b]*row_sum(r,u)*u[w,3] for a in (2:n) for b in (3:n) for c in (2:n) if c != r && a!=3 && (a,c)!=(2,2) &&c!=3 && a!=c)
 +sum(u[3,s]*u[a,t]*u[c,j]*rinj(r,w,u=u) for a in (2:n) for c in (2:n) for j in (3:n) if a!=3 && c!=3 && (a,c)!=(2,2) && j!=t && c != r) # it could be c!=n
-+sum(u[3,s]*u[2,t]*u[c,j]*u[r,d]*row_sum(w,u) for c in (4:n) for d in (3:n) for j in (3:n) if c != r && j!=t)
--sum(u[3,s]*u[2,t]*inj(b,j,r,u=u)*u[w,c] for b in (4:n) for j in (3:n) for c in (2:n) if c != 3 && c!=j && b!=r && j!=t)
--sum(u[3,s]*u[2,t]*u[b,j]*inj(r,c,w,u=u) for j in (3:n) for b in (4:n) for c in (4:n))
++sum(u[3,s]*u[a,t]*u[c,j]*u[r,d]*row_sum(w,u) for a in (2:n) for c in (2:n) for d in (3:n) for j in (3:n) if a!=3 && c != r && j!=t && c!=3 && (a,c)!=(2,2))
+-sum(u[3,s]*u[a,t]*inj(b,j,r,u=u)*u[w,c] for a in (2:n) for b in (2:n) for j in (3:n) for c in (2:n) if a!=3 && c != 3 && c!=j && b!=r && j!=t && b!=3 && (a,b)!=(2,2))
+-sum(u[3,s]*u[a,t]*u[b,j]*inj(r,c,w,u=u) for a in (2:n) for j in (3:n) for b in (2:n) for c in (4:n) if a!=3 && b!=3 && (a,b)!=(2,2))
 +sum(u[3,s]*u[2,t]*ip(r,j,u=u)*u[w,j] for j in (4:n) if j!=t)
 -sum(bg(8,s,t,a,u=u)*u[r,b]*u[w,c] for a in (3:n) for b in (2:n) for c in (2:n) if a != t && c!=b && a!=b && c!=3)
 +sum(u[3,s]*inj(2,t,a,u=u)*u[r,c]*u[w,3] for a in (4:n) for c in (2:n))
 +sum(u[3,s]*inj(2,t,a,u=u)*u[r,c]*u[w,c] for a in (4:n) for c in (4:n))
--sum(u[3,s]*u[2,t]*wel(r,t,b,u=u)*u[w,c] for b in (2:n) for c in (2:n) if b != t && (b,c)!=(2,2))
 +sum(u[3,s]*u[b,t]*col_sum(2,u)*u[r,j]*u[w,3] for b in (3:n) for j in (4:n))
 -sum(u[3,s]*u[b,t]*col_sum(i,u)*u[r,j]*u[w,d] for b in (3:n) for i in (3:n) for j in (2:n) for d in (2:n) if j!=i && d!=3 && d!=j && i!=t)
 -sum(wel(3,s,t,u=u)*u[b,2]*u[r,c]*u[w,3] for b in (2:n) for c in (4:n))
 +sum(wel(3,s,t,u=u)*u[3,2]*u[r,c]*u[w,3] for c in (4:n))
 +sum(wel(3,s,t,u=u)*u[b,i]*u[r,c]*u[w,d] for b in (2:n) for i in (3:n) for c in (2:n) for d in (2:n) if  d!=3 && c!=d && i!=t && b!=3 && i!=c)
--sum(u[3,s]*wel(4,t,2,u=u)*u[r,c]*u[w,3] for c in (4:n))
--sum(u[3,s]*u[4,t]*wel(r,2,c,u=u)*u[w,3] for c in (4:n))
-
+-sum(u[3,s]*wel(4,t,j,u=u)*u[r,c]*u[w,3] for j in (2:n) for c in (2:n) if j!=t && (j,c)!=(2,2))
+-sum(u[3,s]*u[b,t]*wel(r,2,c,u=u)*u[w,3] for c in (4:n) for b in (2:n) if b!=3)
++sum(u[3,s]*u[2,t]*wel(r,a,b,u=u)*u[w,c] for a in (3:n) for b in (2:n) for c in (2:n) if a!=b && c!=3 && (b,c)!=(2,2))
+-sum(u[3,s]*u[2,t]*wel(r,t,b,u=u)*u[w,c] for b in (2:n) for c in (2:n) if b != t && (b,c)!=(2,2))
 ;
-words=["rinj"],
+words=["wel"],
 len=500
 ))
 
