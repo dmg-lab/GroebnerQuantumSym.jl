fc1dbae22aeab214b72b353ac05ad577d3b3e987
Marcel Wack <wack@math.tu-berlin.de>
2025-02-04 15:23:30 +0100

    FINISHED BG8243

--- computation/reductions/bg8243.jl
+++ computation/reductions/bg8243.jl
@@ -1,6 +1,6 @@
 #Reduction of Bg8243
 
-n = 6
+n = 12
 G1 = g1_named(n)
 u = magic_unitary(n)
 bege8243 = bg(8,2,4,3; u=u)
@@ -65,56 +65,62 @@ add!(E1, G1; check=false)
 reduction_string(E1, bege8243
 -sum([E1["bg2_2$(k)3"] for k in 4:n])+sum([E1["bg8_2$(k)3"] for k in 5:n])
 -u[3,2]*E1["cs4"]*u[3,3] +u[3,2]*E1["rwel43"]+E1["wel324"]*u[3,3]
-+help1(3,2)-u[3,2]*E1["rwel43"]+E1["ucsu32433"]-E1["wel325"]*u[3,3] - E1["wel326"]*u[3,3] #Scheinbar nicht fine enough
-+help1(4,2)+help1(5,2)+help1(6,2)
++help1(3,2)-u[3,2]*E1["rwel43"]+E1["ucsu32433"]
+
+-sum([E1["wel32$g"]*u[3,3] for g in 5:n])
+
+
++sum([help1(g,2) for g in 4:n])
+
+
 +sum([sum([E1["ursu2$g$(i)33"] for i in 4:n])-sum([E1["ursu$(h)$g$k$(i)3"] for h in 3:n for i in 2:n for k in 2:n if i != k && k != h && i != 3]) for g in 3:n])
 
 -sum([E1["wesc$h$g$k$i"] for g in 3:n for h in 3:n for k in 2:n for i in 2:n if k != i && k != g && i != 3])
 +sum([u[3,g]*E1["wel3$k$i"] for g in 3:n for k in 2:n for i in 2:n if i != 3 && i != k && k != g])
 
--sum([E1["wel$(g)23"]+4*sum([E1["wel$(g)2$i"] for i in 4:n]) for g in 4:n])
--2*sum([E1["wel$(g)3$i"] for g in 4:n for i in 2:n if i != 3])
-+sum([E1["wel$(g)$(h)3"]-2*sum([E1["wel$(g)$h$i"] for i in 2:n if i != 3 && i != h]) for g in 4:n for h in 4:n])
+-sum([E1["wel$(g)23"]+(n-2)*sum([E1["wel$(g)2$i"] for i in 4:n]) for g in 4:n])
+-(n-4)*sum([E1["wel$(g)3$i"] for g in 4:n for i in 2:n if i != 3])
++sum([E1["wel$(g)$(h)3"]-(n-4)*sum([E1["wel$(g)$h$i"] for i in 2:n if i != 3 && i != h]) for g in 4:n for h in 4:n])
 
 
--4*sum([E1["cs2"]*u[3,i] for i in 4:n])
+-(n-2)*sum([E1["cs2"]*u[3,i] for i in 4:n])
 
-+sum([(4*sum([u[k,2]*E1["cs$i"] for i in 4:n]) -3*u[k,2]*E1["cs3"]) for k in 3:n])
+  +sum([((n-2)*sum([u[k,2]*E1["cs$i"] for i in 4:n]) -(n-3)*u[k,2]*E1["cs3"]) for k in 3:n])
 
-+sum([(6*sum([u[g,h]*E1["cs$i"] for g in 3:n for i in 2:n if i != 3])-3*sum([E1["cs$h"]*u[3,i] for i in 2:n if i != h && i != 3])) for h in 3:n])
++sum([((2*n-6)*sum([u[g,h]*E1["cs$i"] for g in 3:n for i in 2:n if i != 3])-(n-3)*sum([E1["cs$h"]*u[3,i] for i in 2:n if i != h && i != 3])) for h in 3:n])
 
 +sum([E1["cs$g"]*u[3,3] for g in 4:n])
-  -sum([(u[h,g]*E1["cs3"] +2*u[h,g]*E1["cs$g"]) for h in 3:n for g in 4:n])
+  -sum([(u[h,g]*E1["cs3"] +(n-4)*u[h,g]*E1["cs$g"]) for h in 3:n for g in 4:n])
 
--12*E1["cs2"]
-+3*E1["cs3"]
--13*sum([E1["cs$i"] for i in 4:n])
+-(n^2-5*n+6)*E1["cs2"]
+-(n^2-5*n+7)*sum([E1["cs$i"] for i in 4:n])
++(n-3)*E1["cs3"]
 
-+4* sum([E1["rs2"]*u[i,3] for i in 2:n if i != 3 && i != 2])
-+3* sum([E1["rs3"]*u[i,3] for i in 2:n if i != 3 ])
-+sum([(3*sum([E1["rs$g"]*u[i,3] for i in 2:n if i != 3 && i != g]) -E1["rs$g"]*u[3,3]) for g in 4:n])
++(n-2)*sum([E1["rs2"]*u[i,3] for i in 2:n if i != 3 && i != 2])
++(n-3)* sum([E1["rs3"]*u[i,3] for i in 2:n if i != 3 ])
++sum([((n-3)*sum([E1["rs$g"]*u[i,3] for i in 2:n if i != 3 && i != g]) -E1["rs$g"]*u[3,3]) for g in 4:n])
 
-+3*u[2,3]*E1["rs3"]
--3*sum([u[2,3]*E1["rs$i"] for i in 4:n])
-+3*sum([u[2,i]*E1["rs3"] for i in 4:n])
++(n-3)*u[2,3]*E1["rs3"]
+-(n-3)*sum([u[2,3]*E1["rs$i"] for i in 4:n])
++(n-3)*sum([u[2,i]*E1["rs3"] for i in 4:n])
 
 -sum(u[2,3]*E1["rs$i"] for i in 4:n)
 
--6 * sum(u[h,g]*E1["rs$i"] for h in 3:n for g in 3:n for i in 2:n if h != i && i != 3)
+-(2*n-6)*sum(u[h,g]*E1["rs$i"] for h in 3:n for g in 3:n for i in 2:n if h != i && i != 3)
 
--4*sum([u[g,h]*E1["rs$g"] for h in 3:n for g in 4:n])
--4*sum([u[2,h]*E1["rs$g"] for h in 4:n for g in 4:n])
+-(n-2)*sum([u[g,h]*E1["rs$g"] for h in 3:n for g in 4:n])
+-(n-2)*sum([u[2,h]*E1["rs$g"] for h in 4:n for g in 4:n])
 +sum([u[g,h]*E1["rs3"] for g in 4:n for h in 3:n])
 
-+12*E1["rs2"]
-+13*sum([E1["rs$i"] for i in 4:n])
--3*E1["rs3"]
++(n^2-5*n+6)*E1["rs2"]
++(n^2-5*n+7)*sum([E1["rs$i"] for i in 4:n])
+-(n-3)*E1["rs3"]
 
 #rinjes
 +sum([(-sum([u[2,h]*E1["rinj$(i)3"] for i in 4:n])-sum([u[g,h]*E1["rinj$(i)3"] for g in 3:n for i in 2:n if i != 3 && i != g])+sum([u[g,h]*E1["rinj$k$i"] for g in 3:n for k in 2:n  for i in 2:n if  k != g && i != k])) for h in 3:n])
--4*sum([E1["rinj2$i"] for i in 4:n])
--3*sum([E1["rinj$g$i"] for g in 3:n for i in 2:n if i != g])
-+4*sum([E1["rinj$(i)3"] for i in 4:n])
+-(n-2)*sum([E1["rinj2$i"] for i in 4:n])
+-(n-3)*sum([E1["rinj$g$i"] for g in 3:n for i in 2:n if i != g])
++(n-2)*sum([E1["rinj$(i)3"] for i in 4:n])
 
 -sum([E1["bg8_$g$h$k"] for g in 3:n for h in 2:n for k in 2:n if g != h && 3 != k && h != k])
 
@@ -129,17 +135,20 @@ reduction_string(E1, bege8243
 +sum([E1["inj23$k"]*u[3,3] for k in 4:n])
 -sum([u[h,3]*E1["inj$(g)3$k"] for h in 3:n for g in 2:n for k in 2:n if k != g && k != 3 && g != h])
 
-+sum([(E1["inj2$(h)3"]+2*sum([E1["inj2$(h)$k"] for k in 4:n]) +sum([2*sum([E1["inj$(g)$(h)$k"] for k in 2:n if k != 3 && k != g]) for g in 2:n ]) -sum([E1["inj$(g)$(h)3"] for g in 4:n])) for h in 4:n])
++sum([(E1["inj2$(h)3"]
++2*sum([E1["inj2$(h)$k"] for k in 4:n])
++sum([(n-4)*sum([E1["inj$(g)$(h)$k"] for k in 2:n if k != 3 && k != g]) for g in 2:n ]) 
+-sum([E1["inj$(g)$(h)3"] for g in 4:n])) for h in 4:n])
 
 -sum([u[h,j]*E1["rwel$k$i"]  for j in 3:n for h in 3:n for k in 2:n for i in 2:n if k != j && k != i && i != 3]) 
 
-+4*sum([E1["rwel2$i"] for i in 2:n if 2 != i && i != 3])
-+3*sum([E1["rwel$k$i"] for k in 3:n for i in 2:n if k != i && i != 3])
++(n-2)*sum([E1["rwel2$i"] for i in 2:n if 2 != i && i != 3])
++(n-3)*sum([E1["rwel$k$i"] for k in 3:n for i in 2:n if k != i && i != 3])
 - sum([E1["rwel$(h)3"] for h in 4:n])
 
 )
 ,len=40)
-;words=["wesc"])
+;words=["rs"])
 
 
 
@@ -147,4 +156,6 @@ reduction_string(E1, bege8243
 ;len=30)
 ,to_file="../data/reduction_strings/n_6_bege8243.txt")
 
+f(x) =-4*(x-5)+2*x*(x-5)-sum([2*i for i in 1:(x-5)])+6
+g(x) =x^2+-5x +6
 
